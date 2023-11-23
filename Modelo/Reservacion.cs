edef64e8c45c634692a3e40fac838b6ed0b67092
DanielRodriguezC32 <RCoronaDaniel@gmail.com>
2023-11-22 20:15:41 -0800

    Se crean las clases Pago, Reservacion, TipoPago, Transaccion y Vuelo

---
+++ Modelo/Reservacion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class Reservacion
+    {
+        public int ReservacionId { get; set; }
+        public int VueloId { get; set; }
+        public int TransaccionId { get; set; }
+
+    }
+}
---
+++ Modelo/Transaccion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class Transaccion
+    {
+        public int TransaccionId { get; set; }
+        public int PagoId { get; set; }
+        public double Cantidad { get; set; }
+        public DateTime Fecha { get; set; }
+    }
+}
