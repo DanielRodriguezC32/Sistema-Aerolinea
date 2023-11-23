4b1cc3012394ac453d9a8bfef316850fba669f65
Rodrigo Ruiz <rodrigo.ruiz.ortega@gmail.com>
2023-11-22 20:36:25 -0800

    Se agregan proyectos, se establece Entity Framework

---
+++ DataAccessLayer/Datos.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using DataBase;
+using System.Linq;
+
+namespace DataAccessLayer
+{
+    public class Datos
+    {
+        public static List<Modelo.Lugar> ListaLugares()
+        {
+            var lista = new List<Modelo.Lugar>();
+            using (var ctx = new AeropuertoEntitiesRodrigo())
+            {
+                ctx.Lugar.ToList();
+            }
+            return lista;
+        }
+    }
+}
---
+++ DataBase/ctx.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.Entity;
+
+public class ctx : DbContext
+{
+    public DbSet<Usuario> Usuarios { get; set; }
+    public DbSet<Pago> Pagos { get; set; }
+
+    public ctx() : base("AeropuertoEntitiesRodrigo") // Usa el nombre de la cadena de conexión
+    {
+    }
+}
