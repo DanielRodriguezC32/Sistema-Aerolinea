8416f5e7ccd9761a487c88d53f70791cac738db3
Rodrigo Ruiz <rodrigo.ruiz.ortega@gmail.com>
2023-11-22 19:10:08 -0800

    Se agrega clase Usuario

---
+++ Modelo/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class Usuario
+    {
+        public int UsuarioId { get; set; }
+        public string Nombres { get; set; }
+        public string ApellidoPaterno { get; set; }
+        public string ApellidoMaterno { get; set; }
+    }
+}
