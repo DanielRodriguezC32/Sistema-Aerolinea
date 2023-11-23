89686e36a77042958200d61c13ec634881c1e026
Rodrigo Ruiz <rodrigo.ruiz.ortega@gmail.com>
2023-11-22 18:35:40 -0800

    Se agrega nuevo proyecto y una clase de muestra

---
+++ Modelo/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Modelo
+{
+    public class Lugar
+    {
+        public int LugarId { get; set; }
+        public string Descripcion { get; set; }
+        public string Clave { get; set; }
+        
+    }
+}
