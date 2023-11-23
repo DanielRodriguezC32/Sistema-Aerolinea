dba3d566009f2ba84a5da069af42bde40ec486f0
Rodrigo Ruiz <rodrigo.ruiz.ortega@gmail.com>
2023-11-22 21:47:01 -0800

    cambios commit mientras probaba con daniel

--- DataBase/ctx.cs
+++ DataBase/ctx.cs
@@ -2,8 +2,8 @@
 
 public class ctx : DbContext
 {
-    public DbSet<Usuario> Usuarios { get; set; }
-    public DbSet<Pago> Pagos { get; set; }
+    //public DbSet<Usuario> Usuarios { get; set; }
+    //public DbSet<Pago> Pagos { get; set; }
 
     public ctx() : base("AeropuertoEntitiesRodrigo") // Usa el nombre de la cadena de conexión
     {
